dd100cb0e8739ba4ef85bcb4f919f236fc3be2df
gabriel <gabriel.stocco06@gmail.com>
2025-05-02 10:21:20 -0300

    Classe Emrpesa

---
+++ src/com/sf/main/Empresa.java
@@ -0,0 +1,37 @@
+package com.sf.main;
+
+public class Empresa {
+	private int empresa;
+	private String endereco_Emp;
+	private int cnpj_Emp;
+	private String nome_Emp;
+	
+	public int getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(int empresa) {
+		this.empresa = empresa;
+	}
+	public String getEndereco_Emp() {
+		return endereco_Emp;
+	}
+	public void setEndereco_Emp(String endereco_Emp) {
+		this.endereco_Emp = endereco_Emp;
+	}
+	public int getCnpj_Emp() {
+		return cnpj_Emp;
+	}
+	public void setCnpj_Emp(int cnpj_Emp) {
+		this.cnpj_Emp = cnpj_Emp;
+	}
+	public String getNome_Emp() {
+		return nome_Emp;
+	}
+	public void setNome_Emp(String nome_Emp) {
+		this.nome_Emp = nome_Emp;
+	}
+	
+
+	
+	
+}
