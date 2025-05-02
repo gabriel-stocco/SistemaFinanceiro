6f7c5e0db674dbb57d648f56f0bf8da0fa7f30d0
vicenzodev <heitor@vicenzo.dev>
2025-05-02 10:07:09 -0300

    Abrindo a primeira tela

--- src/com/sf/main/Main.java
+++ src/com/sf/main/Main.java
@@ -1,5 +1,32 @@
 package com.sf.main;
 
-public class Main {
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	
+	/**
+	 * Código de serialização gerado automaticamente
+	 */
+	private static final long serialVersionUID = -1982822269813614355L;
+	
+	/*
+	 * Função principal que instancia a classe e abre a janela
+	 */
+	public static void main(String args[]) {
+		Main main = new Main();
+		main.start();
+	}
+	
+	/*
+	 * Função Start que inicia os parâmetros da tela do sistema
+	 */
+	private void start() {
+		setTitle("Sistema Financeiro");
+		setBounds(0,0,500,500);
+		setLayout(new FlowLayout());
+		setVisible(true);
+	}
 
 }
